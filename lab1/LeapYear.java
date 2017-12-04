b0943fcd0f14598e3b54a269983cf53d8c7f90f4
songzhentian <songzhentian1993@gmail.com>
2017-12-04 14:44:54 +0800

    Improve the function of LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author songzhentian
  */
 public class LeapYear {
 
@@ -14,6 +14,13 @@ public class LeapYear {
         }
     }
 
+	public static boolean isLeapYear(int year) {
+		if ((year % 400 == 0) || ((year % 4 == 0 ) && (year % 100 != 0))) {
+			return true;
+		}
+		return false;
+	}
+	
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
