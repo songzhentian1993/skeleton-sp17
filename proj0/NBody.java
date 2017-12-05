44c86f02f94700bb9ee91bde5182f4771b16ca08
songzhentian <songzhentian1993@gmail.com>
2017-12-05 17:06:51 +0800

    Add animation to NBody.java and had my first project(proj0) completed!

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -26,20 +26,35 @@ public class NBody {
 		}
 		return planets;
 	}
-	
-	//public static void drawBackground() {}
-	
+
 	public static void main(String[] args) {
+		double time = 0;
 		double T = Double.parseDouble(args[0]);
 		double dt = Double.parseDouble(args[1]);
 		String filename = args[2];
 		double radius = readRadius(filename);
 		Planet[] planets = readPlanets(filename);
 		StdDraw.setScale(-radius, radius);
-		StdDraw.picture(0, 0, "./images/starfield.jpg");
-		for (Planet p: planets) {
-			p.draw();
+		//StdDraw.enableDoubleBuffering();
+		while (time < T) {
+			double[] arr_xForce = new double[planets.length];
+			double[] arr_yForce = new double[planets.length];
+			for (int i = 0; i < planets.length; i++) {
+				double NFx = planets[i].calcNetForceExertedByX(planets);
+				double NFy = planets[i].calcNetForceExertedByY(planets);
+				arr_xForce[i] = NFx;
+				arr_yForce[i] = NFy;
+			}
+			for (int i = 0; i < planets.length; i++) {
+				planets[i].update(dt, arr_xForce[i], arr_yForce[i]);
+			}
+			StdDraw.picture(0, 0, "./images/starfield.jpg");
+			for (Planet p: planets) {
+				p.draw();
+			}
+			StdDraw.show(10);
+			//StdDraw.pause(10);
+			time = time + dt;
 		}
-		StdDraw.show();
 	}
 }
