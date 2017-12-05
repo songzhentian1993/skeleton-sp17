628cd6550abb925f06e970eab105ce5c32231964
songzhentian <songzhentian1993@gmail.com>
2017-12-05 16:00:24 +0800

    Add main to NBody.java and draw to Planet.java

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -19,11 +19,27 @@ public class NBody {
 			double xxVel = in.readDouble();
 			double yyVel = in.readDouble();
 			double mass = in.readDouble();
-			String imgFileName = in.readString();
+			String imgFileName = "./images/" + in.readString();
 			Planet p = new Planet(xxPos, yyPos, xxVel, yyVel, mass, imgFileName);
 			planets[count] = p;
 			count = count + 1;
 		}
 		return planets;
+	}
+	
+	//public static void drawBackground() {}
+	
+	public static void main(String[] args) {
+		double T = Double.parseDouble(args[0]);
+		double dt = Double.parseDouble(args[1]);
+		String filename = args[2];
+		double radius = readRadius(filename);
+		Planet[] planets = readPlanets(filename);
+		StdDraw.setScale(-radius, radius);
+		StdDraw.picture(0, 0, "./images/starfield.jpg");
+		for (Planet p: planets) {
+			p.draw();
+		}
+		StdDraw.show();
 	}
 }
--- proj0/Planet.java
+++ proj0/Planet.java
@@ -93,4 +93,8 @@ public class Planet {
 		xxPos = xxPos + dt * xxVel;
 		yyPos = yyPos + dt * yyVel;
 	}
+	
+	public void draw() {
+		StdDraw.picture(xxPos, yyPos, imgFileName);
+	}
 }
